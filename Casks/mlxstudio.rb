31aefd659bf65ef0d55272964f4c45e08b37f8dc
Jinho Jang <eric@jangq.ai>
2026-08-25 23:39:26 -0700

    Update MLX Studio cask to 1.6.40

--- Casks/mlxstudio.rb
+++ Casks/mlxstudio.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "mlxstudio" do
-  version "1.6.39"
-  sha256 "193bce94060c13be7df9d7837a6c59e81dc6f015c0530de6af3ee4fa97c1fd80"
+  version "1.6.40"
+  sha256 "4480e0cba1f426dfa8964272a912c75712e5f69831a9169737b9e5048a2bfad7"
 
   url "https://github.com/jjang-ai/mlxstudio/releases/download/v#{version}/vMLX-#{version}-sequoia-arm64.dmg"
   name "vMLX"
